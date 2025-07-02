8d5f84dc8359242fa1d4eddfd63f11bbe8978af8
Zohamo <thiazsch@gmail.com>
2025-07-02 15:16:31 +0200

    fix(LoiDeRootSommaire): close drawer when nav prev/next

--- src/components/LoiDeRootSommaire.vue
+++ src/components/LoiDeRootSommaire.vue
@@ -1,23 +1,12 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import Button from 'primevue/button'
 import Drawer from 'primevue/drawer'
 import Menu from 'primevue/menu'
 import loiDeRoot from '../assets/json/loi-de-root.json'
-
-const props = defineProps({ hide: Boolean })
-const emit = defineEmits(['hide'])
+import { onBeforeRouteUpdate } from 'vue-router'
 
 const visible = ref(false)
-
-// FIXME j'avais pensé à un truc plus simple mais j'ai oublié
-// en tout cas ça déconne quand on navigue avec prev/next puis le sommaire
-// le drawer ne se ferme plus
-watch(props, () => {
-  if (props.hide) {
-    visible.value = false
-  }
-})
 
 const items = ref(
   loiDeRoot.map((section) => {
@@ -25,11 +14,16 @@ const items = ref(
   }),
 )
 items.value.unshift({ label: 'Lire la Loi', route: '/loi-de-root' })
+
+onBeforeRouteUpdate(async () => {
+  visible.value = false
+  window.scroll({ top: 0 })
+})
 </script>
 
 <template>
   <div class="fixed top-16 right-2">
-    <Drawer v-model:visible="visible" @hide="emit('hide')" header="Sommaire" position="right">
+    <Drawer v-model:visible="visible" header="Sommaire" position="right">
       <Menu :model="items">
         <template #item="{ item, props }">
           <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
