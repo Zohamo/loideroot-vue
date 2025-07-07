a2f7db8dbfc3d85d1c60dc2c0a40962aec359dce
Zohamo <thiazsch@gmail.com>
2025-07-07 10:23:23 +0200

    config(path): set base directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), VitePWA({ registerType: 'autoUpdate' })],
+  base: '/root',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
